91c77522c5ab128a3a59d04355845ef1ee0fa26f
calmlyx <calmlyx@qq.com>
2020-03-04 21:17:16 +0800

    0

--- src/views/Register/index.vue
+++ src/views/Register/index.vue
@@ -1,8 +1,9 @@
 <template>
     <div class="register">
-        <div class="head"></div>
+        <Header title="注册" style="background: #51ca89;color: white"/>
+        <!--<div class="head"></div>
         <van-nav-bar title="注册" left-arrow @click-left="$router.back()">
-        </van-nav-bar>
+        </van-nav-bar>-->
         <form class="registerForm">
             <div class="phoneText">
                 <img src="../../assets/Register/eamilIcon@2x.png">
@@ -29,97 +30,111 @@
             <van-field name="radio">
                 <template #input>
                     <van-radio-group v-model="radio">
-                        <van-radio name="1"><p>我已阅读并同意<router-link to="login">《用户协议》</router-link></p></van-radio>
+                        <van-radio name="1">
+                            <p>我已阅读并同意
+                                <router-link to="login">《用户协议》</router-link>
+                            </p>
+                        </van-radio>
                     </van-radio-group>
                 </template>
             </van-field>
         </form>
-
-
-
     </div>
 </template>
 
 <script>
-    export default {
-        name: "index",
-        data() {
-            return {
-                username: '',
-                password: '',
-                sms:'',
-                radio: '0',
-            };
+import Header from "../../components/Header"
+export default {
+    name: "index",
+    data() {
+        return {
+            username: '',
+            password: '',
+            sms: '',
+            radio: '0',
+        };
+    },
+    components: {
+        Header
+    },
+    methods: {
+        onSubmit(values) {
+            console.log('submit', values);
         },
-        methods: {
-            onSubmit(values) {
-                console.log('submit', values);
-            },
-        },
+    },
 
 
-    }
+}
 </script>
 
 <style scoped>
-    .head{
+    .head {
         width: 100%;
         height: 35px;
     }
-    .van-nav-bar
-    {
+
+    .van-nav-bar {
         background: #51ca89;
     }
-    .van-icon-arrow-left::before{
+
+    .van-icon-arrow-left::before {
         color: white;
     }
+
     .van-nav-bar__text,
-    .van-nav-bar__title{
+    .van-nav-bar__title {
         color: white;
         font-family: SourceHanSansCN-Regular;
         font-size: 18px;
     }
-    .registerForm{
+
+    .registerForm {
         width: 85%;
         height: 315px;
-        margin:0 auto;
+        margin: 0 auto;
         margin-top: 57px;
     }
-    .registerForm>div{
+
+    .registerForm > div {
         display: flex;
         margin: 15px 10px;
     }
-    .registerForm img{
+
+    .registerForm img {
         width: 19px;
         height: 22px;
     }
-    .Splitter{
+
+    .Splitter {
         width: 91%;
         height: 1px;
         margin: 0 auto;
         background-color: #dcdcdc;
     }
-    .codeInput{
+
+    .codeInput {
         width: 80%;
         border: none;
         position: relative;
-        left:10px;
+        left: 10px;
         font-family: SourceHanSansCN-Regular;
         font-size: 15px;
         line-height: 19px;
         color: #999999;
     }
-    .phoneInput,.passwordInput{
+
+    .phoneInput, .passwordInput {
         width: 80%;
         border: none;
         position: relative;
-        left:10px;
+        left: 10px;
         font-family: SourceHanSansCN-Regular;
         font-size: 15px;
         line-height: 19px;
         color: #999999;
     }
-    .codeText button{
+
+    .codeText button {
         background-color: white;
         border: 1px #51ca89 solid;
         border-radius: 11px;
@@ -130,25 +145,31 @@
         font-size: 12px;
         line-height: 14px;
     }
-    .registerButton{
+
+    .registerButton {
         margin-top: 50px !important;
     }
-    .registerButton button{
+
+    .registerButton button {
         background-color: #51ca89 !important;
     }
+
     .van-radio__icon .van-icon {
         color: #51ca89;
         width: 14px !important;
         height: 14px !important;
     }
-    van-icon van-icon-success{
+
+    van-icon van-icon-success {
         background-color: #51ca89 !important;
         border-color: #51ca89 !important;
     }
-    .van-radio-group{
+
+    .van-radio-group {
         margin: 0 auto !important;
     }
-    .van-radio__label p{
+
+    .van-radio__label p {
         font-family: SourceHanSansCN-Regular !important;
         font-size: 12px !important;
         line-height: 14px !important;
